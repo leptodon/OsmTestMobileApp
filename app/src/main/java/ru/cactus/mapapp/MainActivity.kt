4b8da0c564bcc83c6c66a468a58024a5f886f8eb
Cactus <dev.cactus.group@gmail.com>
2022-08-13 17:00:23 +0200

    - change mockMarker function

--- app/src/main/java/ru/cactus/mapapp/MainActivity.kt
+++ app/src/main/java/ru/cactus/mapapp/MainActivity.kt
@@ -1,13 +1,16 @@
 package ru.cactus.mapapp
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.osmdroid.api.IMapController
@@ -38,6 +41,7 @@ class MainActivity : AppCompatActivity() {
     private var markers: MutableList<Marker> = mutableListOf()
     private var currentMarkerIndex: Int = 0
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -71,7 +75,14 @@ class MainActivity : AppCompatActivity() {
 
 
         val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), map)
+        val bitmapArrow =
+            ResourcesCompat.getDrawable(resources, R.drawable.ic_my_tracker_46dp, theme)?.toBitmap()
+        locationOverlay.setDirectionIcon(bitmapArrow)
         locationOverlay.enableMyLocation()
+
+        binding.btnCurrentLocation.setOnClickListener {
+            locationOverlay.enableFollowLocation()
+        }
 
         map.overlays.add(locationOverlay)
 
@@ -79,17 +90,20 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.location.observe(this) { location ->
             mapController?.setCenter(GeoPoint(location.latitude, location.longitude))
-            binding.btnCurrentLocation.setOnClickListener {
-                mapController?.animateTo(
-                    GeoPoint(
-                        location.latitude,
-                        location.longitude
-                    )
-                )
-                mapController?.zoomIn(10)
-                mapController?.setZoom(20.0)
-            }
+
+//            binding.btnCurrentLocation.setOnClickListener {
+//
+//                mapController?.animateTo(
+//                    GeoPoint(
+//                        location.latitude,
+//                        location.longitude
+//                    )
+//                )
+//                mapController?.setZoom(18.0)
+//            }
         }
+
+        setMockMarkers()
     }
 
     override fun onStart() {
@@ -102,7 +116,6 @@ class MainActivity : AppCompatActivity() {
             Manifest.permission.ACCESS_COARSE_LOCATION,
             COARSE_LOCATION_CODE
         )
-        setMockMarkers()
     }
 
     override fun onResume() {
@@ -117,28 +130,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun setMockMarkers() {
         viewModel.location.observe(this) {
-            for (location in 0..5) {
-                val marker = Marker(map)
-                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
-                marker.icon =
-                    ResourcesCompat.getDrawable(resources, R.drawable.custom_marker, theme)
-                marker.setOnMarkerClickListener { _, _ ->
-                    dialog()
-                    true
+            if (markers.isEmpty()) {
+                for (location in 0..5) {
+                    val marker = Marker(map)
+                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+                    marker.icon =
+                        ResourcesCompat.getDrawable(resources, R.drawable.custom_marker, theme)
+                    marker.setOnMarkerClickListener { _, _ ->
+                        dialog()
+                        true
+                    }
+                    marker.position = GeoPoint(
+                        it.latitude + Random.nextDouble(0.1, 0.9),
+                        it.longitude + Random.nextDouble(0.1, 0.9)
+                    )
+
+                    marker.infoWindow = MyInfoWindow(map)
+
+                    map.overlays.add(marker)
+                    markers.add(marker)
                 }
-                marker.position = GeoPoint(
-                    it.latitude + Random.nextDouble(0.1, 0.9),
-                    it.longitude + Random.nextDouble(0.1, 0.9)
-                )
-
-                marker.infoWindow = MyInfoWindow(map)
-
-                map.overlays.add(marker)
-                markers.add(marker)
             }
-            map.invalidate()
         }
-
     }
 
     private fun dialog() {
@@ -189,4 +202,5 @@ class MainActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT
         ).show()
     }
+
 }
--- app/src/main/java/ru/cactus/mapapp/MainViewModel.kt
+++ app/src/main/java/ru/cactus/mapapp/MainViewModel.kt
@@ -2,10 +2,15 @@ package ru.cactus.mapapp
 
 import android.annotation.SuppressLint
 import android.location.Location
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
 
 @SuppressLint("MissingPermission")
 class MainViewModel(
@@ -15,12 +20,31 @@ class MainViewModel(
     private val _location = MutableLiveData<Location>()
     val location: LiveData<Location> = _location
 
+    init {
+        setupLocation()
+    }
+
     fun setupLocation() {
         locationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
                 _location.postValue(location)
             }
+            .addOnCompleteListener {
+                _location.postValue(it.result)
+            }
+
+        locationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+            override fun onCanceledRequested(p0: OnTokenCanceledListener) =
+                CancellationTokenSource().token
+
+            override fun isCancellationRequested() = false
+        })
+            .addOnCompleteListener {
+                if (it.result != null) _location.postValue(it.result) }
+            .addOnSuccessListener {
+                    location: Location? ->
+                if (location != null) _location.postValue(location)
+            }
+
     }
-
-
 }
